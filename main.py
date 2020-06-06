5ff8d54b7c9026ddad22c09b6e594924815179de
mollonn_ubuntu17.10 <vadik.medvedyuk@gmail.com>
2020-06-06 23:46:16 +0300

    trikoz_laba4

--- main.py
+++ main.py
@@ -2,14 +2,7 @@ import telebot
 
 bot = telebot.TeleBot('1184393666:AAF688UCMz5JUOyjE98gdI9jqurxXSgkdq8')
 
-breakage = {'Cтучит подвеска': ['Поменть резину', 'Проверить развал/схождение', 'Заменить подшипник'],
-            'Гул двигателя во время езды': ['Вам необходимо обратиться в автосервис'],
-            'Горит лампочка масла': ['Вам необходимо купить масло и заменить его'],
-            'Горит лампочка аккумулятора': ['Вам необходимо купить аккумулятор и заменить его']}
-node = {'Ходовая': ['Поменть резину', 'Проверить развал/схождение', 'Заменить подшипник'],
-        'Двигатель': ['Вам необходимо обратиться в автосервис'],
-        'Коробка': ['Вам необходимо обратиться в автосервис'],
-        'Расходники': ['Замена масла двигателя', 'Замена масла коробки', 'Замена аккумулятора']}
+node = {'Категория' :['A, B, C - 1$']}
 
 
 def createKeyboard(dict):
@@ -31,28 +24,19 @@ def printRes(l):
 def start_message(message):
     keyboard = telebot.types.InlineKeyboardMarkup()
 
-    keyboard.add(telebot.types.InlineKeyboardButton(text='Найти деталь по узлу', callback_data='searchByNode'))
-    keyboard.add(telebot.types.InlineKeyboardButton(text='Найти деталь по типу поломки', callback_data='searchByType'))
-    bot.send_message(message.chat.id, 'Привет, выберите как искать деталь', reply_markup=keyboard)
+    keyboard.add(telebot.types.InlineKeyboardButton(text='Узнать стоимость обучения', callback_data='courses_cost'))
+    bot.send_message(message.chat.id, 'Выберите действие', reply_markup=keyboard)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def main_query_handler(call):
     bot.answer_callback_query(callback_query_id=call.id)
 
-    if call.data == 'searchByType':
-        typeKey = createKeyboard(breakage)
-        bot.send_message(call.message.chat.id, '\nВыберите тип поломки:', reply_markup=typeKey)
-        bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
-
-    elif call.data in breakage.keys():
-        result = printRes(breakage.get(call.data))
-        bot.send_message(call.message.chat.id, result)
-
-    elif call.data == 'searchByNode':
+    if call.data == 'courses_cost':
         typeKey = createKeyboard(node)
-        bot.send_message(call.message.chat.id, '\nВыберите тип узла:', reply_markup=typeKey)
+        bot.send_message(call.message.chat.id, '\nстоимость:', reply_markup=typeKey)
         bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
+
 
     elif call.data in node.keys():
         result = printRes(node.get(call.data))
